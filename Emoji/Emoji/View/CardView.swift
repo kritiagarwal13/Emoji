4e6297868786d3443f4e7d643cd0f3225396700a
Kriti Agarwal <kriti.agarwal4709@gmail.com>
2023-11-18 13:20:49 +0000

    Adds Dark Mode

---
+++ Emoji/Emoji/View/CardView.swift
@@ -0,0 +1,32 @@
+//
+//  CardView.swift
+//  Emoji
+//
+//  Created by Kriti Agarwal on 18/11/23.
+//
+
+import Foundation
+import UIKit
+
+class CardView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupUI()
+    }
+    
+    private func setupUI() {
+        backgroundColor = UIColor.systemBackground
+        layer.cornerRadius = 10
+        layer.masksToBounds = false
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.2
+        layer.shadowOffset = CGSize(width: 0, height: 2)
+        layer.shadowRadius = 4
+    }
+}
